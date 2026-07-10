03e604579ca846915be4a8ccdba69c84f8fd91eb
Kevin-022C <keyfrans96@gmail.com>
2026-07-09 21:53:52 -0500

    Asaña: fase 1 de centinela principal y su logica completa

--- Assets/Scripts/AI/BossProjectile.cs
+++ Assets/Scripts/AI/BossProjectile.cs
@@ -6,28 +6,44 @@ public class BossProjectile : MonoBehaviour
 
     public float lifeTime = 5f;
 
+    public bool tracking = false;
+
+    public float turnSpeed = 45f;
+
+    private Transform rover;
+
     private void Start()
     {
         Destroy(gameObject, lifeTime);
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+            rover = player.transform;
     }
 
     private void Update()
+{
+    Debug.Log($"Speed = {speed}");
+
+    if (tracking && rover != null)
     {
-        transform.position += transform.right * speed * Time.deltaTime;
+        Vector2 direction =
+            (rover.position - transform.position).normalized;
+
+        float targetAngle =
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        float currentAngle = transform.eulerAngles.z;
+
+        float angle = Mathf.MoveTowardsAngle(
+            currentAngle,
+            targetAngle,
+            turnSpeed * Time.deltaTime);
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // Aquí luego haremos el daño.
-
-            Destroy(gameObject);
-        }
-
-        if (other.CompareTag("Wall"))
-        {
-            Destroy(gameObject);
-        }
-    }
+    transform.position += transform.right * speed * Time.deltaTime;
+}
 }
